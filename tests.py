6a52ea7c685b3e3b14fe3ab72c5c632137524b47
Jonas <jonaslindstrom0000@gmail.com>
2022-07-02 22:47:26 +0100

    Added missing unit tests for the beta and gamma rotors

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ class RotorTests(TestCase):
     """Tests the rotor functionality."""
 
     def test_valid_alpha_rotor(self):
-        """Test the alpha rotor."""
+        """Test the alpha rotor shifts the correct value, and increments."""
         # Used to track the incrementing alpha rotor
         offset_increment = 0
 
@@ -26,5 +26,28 @@ class RotorTests(TestCase):
         expected_value = chr(ord('z') + offset_increment - 25)
         self.assertEqual(rotor.get_shifted_value('z'), expected_value)
 
-main(verbosity = 2)
+    def test_valid_beta_gamma_rotor(self):
+        """Tests that the beta and gamma rotors work as expected."""
+        # Because gamma, and beta call the same code, we can just test one
+        # fully and loosely test the other
+        rotor = Rotor('b', RotorType.BETA)
+        expected_value = 'c'
+        # Tests the initial valid case
+        self.assertEqual(rotor.get_shifted_value('b'), expected_value)
+    
+        # Tests that the offset hasn't increased 
+        self.assertEqual(rotor.get_shifted_value('b'), expected_value)
+
+        # Tests that the edge case works as expected
+        expected_value = 'a'
+        self.assertEqual(rotor.get_shifted_value('z'), expected_value)
         
+        # Tests that the gamma rotor works the same as beta
+        rotor = Rotor('b', RotorType.GAMMA)
+        expected_value = 'c'
+        self.assertEqual(rotor.get_shifted_value('b'), expected_value)
+
+
+
+
+main(verbosity = 2)
